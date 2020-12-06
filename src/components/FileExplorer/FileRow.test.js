7e0e92e0da9fd91c715e533d04d6cfa7cf77d11b
Janluke <gianlucacaronte@gmail.com>
2020-12-07 00:17:16 +0100

    add FileRow basic test

---
+++ src/components/FileExplorer/FileRow.test.js
@@ -0,0 +1,35 @@
+
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import FileRow from "./FileRow.js"
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("tbody");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("check text", () => {
+  act(() => {
+    render(
+      <FileRow
+        Key="FAKE"
+        Name="file.txt"
+        Size={1024}
+        Owner={{ DisplayName: "Giovanni Mucciaccia" }}
+      />, container)
+    expect(container.querySelector("[title='file name']").textContent).toBe("file.txt")
+    expect(container.querySelector("[title='file owner']").textContent).toBe("Giovanni Mucciaccia")
+    expect(container.querySelector("[title='file size']").textContent).toBe("1 kB")
+  });
+})
