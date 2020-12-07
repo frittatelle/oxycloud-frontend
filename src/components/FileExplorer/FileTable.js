fb17682fd6a2a95ef46e5d3b0b8199426b9a35bc
Janluke <gianlucacaronte@gmail.com>
2020-12-07 14:59:56 +0100

    remove unused style code

--- src/components/FileExplorer/FileTable.js
+++ src/components/FileExplorer/FileTable.js
@@ -4,14 +4,13 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 
 import FileRow from "./FileRow"
 import FolderRow from "./FolderRow"
 
 const FileTable = ({ folders, files }) => {
   return (
-    <TableContainer component={Paper}>
+    <TableContainer>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
