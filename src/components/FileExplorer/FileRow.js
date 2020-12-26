2e752a2ab2b477a5fa7c78433ffa31b8bb39afae
Nuccio <nuccio.nc96@gmail.com>
2020-12-26 20:52:23 +0100

    fixed again

--- src/components/FileExplorer/FileRow.js
+++ src/components/FileExplorer/FileRow.js
@@ -10,11 +10,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import GetAppIcon from '@material-ui/icons/GetApp';
 import ShareIcon from '@material-ui/icons/Share';
 import IconButton from '@material-ui/core/IconButton';
-
+/*
 const getIcon = (ext) => {
   for (var type in data)
     if (ext === type.ext)
       return <FontAwesomeIcon className="my-icon" icon={FaIcons[type.icon]} />
+  return <FontAwesomeIcon className="my-icon" icon={FaIcons['faFile']} />
+}
+*/
+const getIcon=(ext)=>{
+  for(const[index,value] of data.entries()){
+    
+    if(value.ext===ext){
+      
+      return <FontAwesomeIcon className="my-icon" key={index} icon={FaIcons[value.icon]} />
+    }
+  }
   return <FontAwesomeIcon className="my-icon" icon={FaIcons['faFile']} />
 }
 
