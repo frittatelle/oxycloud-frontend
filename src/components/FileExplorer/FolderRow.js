59ba078cfa133079f2e7611cc0bbf2c723829adb
Janluke <gianlucacaronte@gmail.com>
2021-12-14 19:06:17 +0100

    remove sharing button from folders

--- src/components/FileExplorer/FolderRow.js
+++ src/components/FileExplorer/FolderRow.js
@@ -3,8 +3,6 @@ import TableRow from '@material-ui/core/TableRow';
 import Link from '@material-ui/core/Link'
 import {faFolderOpen} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import ShareIcon from '@material-ui/icons/Share';
-import IconButton from '@material-ui/core/IconButton';
 
 const FolderRow = ({ path, name, on_share: share, on_change_folder: change_folder }) => {
   return (
@@ -18,7 +16,6 @@ const FolderRow = ({ path, name, on_share: share, on_change_folder: change_folde
       <TableCell align="right">-</TableCell>
       <TableCell align="right">-</TableCell>
       <TableCell align="right">
-        <IconButton onClick={() => share(null)}><ShareIcon fontSize='small' /></IconButton>
       </TableCell>
     </TableRow>
   )
