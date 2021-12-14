2f955380c11f1bf6c4470ac83431aaad91d1436d
Janluke <gianlucacaronte@gmail.com>
2021-12-14 21:01:28 +0100

    fix folders bar

--- src/components/FileExplorer/FoldersBar.js
+++ src/components/FileExplorer/FoldersBar.js
@@ -2,9 +2,9 @@ import Link from '@material-ui/core/Link'
 import Breadcrumbs from '@material-ui/core/Breadcrumbs'
 
 const FoldersBar = ({ currentFolder, setCurrentFolder }) => {
-  const f = currentFolder.split('/');
+  const f = (currentFolder+"/").split('/');
   var folders = [];
-
+  
   folders.push(
     <Link color='inherit' key="" full_path=""
       onClick={(arg) => {
@@ -14,7 +14,7 @@ const FoldersBar = ({ currentFolder, setCurrentFolder }) => {
         </Link>
   );
   for (var i = 0; i < f.length - 1; i++) {
-    var tmp = f.slice(0, i + 1).join("/") + "/";
+    var tmp = f.slice(0, i + 1).join("/") ;
     folders.push(
       <Link color='inherit' key={tmp} full_path={tmp}
         onClick={(arg) => setCurrentFolder(arg.target.attributes['full_path'].value)}>
